1082204245637f4a10603c3998c77f09cf0ee72c
srikanth000 <margamsrikanth7@gmail.com>
2017-12-08 17:02:43 +0530

    statemets getting chaged

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,7 +3,7 @@ module SessionsHelper
     	session[:user_id] = user.id
   end
    def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+   @current_user = @current_user || User.find_by(id: session[:user_id])
   end
    def logged_in?
     !current_user.nil?
